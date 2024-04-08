1414b837b32006f88f9cfdcd5f80fdd2433b9a7b
Dominic Boutet <dominic.bo@hotmail.com>
2024-04-08 09:43:38 -0400

    Cleaned some files

--- pystorm/__init__.py
+++ pystorm/__init__.py
@@ -2,4 +2,3 @@ __all__ = ["utils","welch_psd"]
 
 from pystorm import welch_psd
 from .welch_psd import *
-#from .utils import minitorch
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 with open("README.md", 'r') as f:
     long_description = f.read()
 
